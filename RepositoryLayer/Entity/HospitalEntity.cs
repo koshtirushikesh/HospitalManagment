f65742e3f8f258f705afbbf1a2200693cd55a6f3
koshtirushikesh <koshtirushikesh3@gmail.com>
2024-01-23 15:33:28 +0530

    adding tha hospital migration and updaing the data base

---
+++ RepositoryLayer/Entity/HospitalEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Entity
+{
+    public class HospitalEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   
+        public int HospitalID { get; set; }
+        public string HospitalName { get; set; }
+        public string HospitalAddress { get; set; }
+        public string HospitalCity { get; set; }
+    }
+}
