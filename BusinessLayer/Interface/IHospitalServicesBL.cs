38ee73e103097528036a3a03a5387aa2a56087ee
koshtirushikesh <koshtirushikesh3@gmail.com>
2024-01-23 17:05:15 +0530

    Added :  add doctor in BL layer

---
+++ BusinessLayer/Interface/IHospitalServicesBL.cs
@@ -0,0 +1,15 @@
+﻿using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface IHospitalServicesBL
+    {
+        public DoctorEntity AddDoctors(DoctorEntity doctorEntity);
+    }
+}
+
---
+++ BusinessLayer/Services/HospitalServicesBL.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Interface;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class HospitalServicesBL:IHospitalServicesBL
+    {
+        private readonly IHospitalServices hospitalServices;
+        public HospitalServicesBL(IHospitalServices hospitalServices)
+        {
+            this.hospitalServices = hospitalServices;
+        }
+
+        public DoctorEntity AddDoctors(DoctorEntity doctorEntity)
+        {
+            return hospitalServices.AddDoctors(doctorEntity);
+        }
+    }
+}
