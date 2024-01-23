f8faabd532f4d724fbd40a46683e4265bb3e1553
koshtirushikesh <koshtirushikesh3@gmail.com>
2024-01-23 16:47:46 +0530

    Add doctor in RL Layer For Hospital Services

---
+++ RepositoryLayer/Services/HosptialServices.cs
@@ -0,0 +1,41 @@
+﻿using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Services
+{
+    public class HosptialServices
+    {
+        public readonly HospitalManagmentContext hospitalManagmentContext;
+        public HosptialServices(HospitalManagmentContext hospitalManagmentContext)
+        {
+            this.hospitalManagmentContext = hospitalManagmentContext;
+        }
+
+        public DoctorEntity AddDoctors(DoctorEntity doctorEntity)
+        {
+            DoctorEntity doctor = hospitalManagmentContext.Doctors.Where( x => x.DoctorName == doctorEntity.DoctorName).FirstOrDefault();
+
+            if (doctor == null)
+            {
+                DoctorEntity doc = new DoctorEntity
+                {
+                    DoctorName = doctorEntity.DoctorName,
+                    Qualification = doctorEntity.Qualification,
+                    salary = doctorEntity.salary
+                };
+                hospitalManagmentContext.Doctors.Add(doc);
+                hospitalManagmentContext.SaveChanges();
+
+                return doc;
+            }
+
+            return null;
+        }
+    }
+}
