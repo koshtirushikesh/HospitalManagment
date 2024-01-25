950c3aa4fa525ea9124b7f545182d46ccdaf74b1
koshtirushikesh <koshtirushikesh3@gmail.com>
2024-01-25 14:47:43 +0530

    Add: adding the migration for Patient login table Email and Password

--- RepositoryLayer/Entity/PatientEntity.cs
+++ RepositoryLayer/Entity/PatientEntity.cs
@@ -19,6 +19,8 @@ namespace RepositoryLayer.Entity
         public int HospitalID { get; set; }
         [ForeignKey("Doctors")]
         public int DoctorID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
     }
 }
